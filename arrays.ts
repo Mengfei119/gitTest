4e6a6910a593435c43ce96446fee45e64861a36d
Mengfei119 <43018961+Mengfei119@users.noreply.github.com>
2020-05-13 23:20:52 -0400

    Update arrays.ts

--- arrays.ts
+++ arrays.ts
@@ -2,8 +2,10 @@ let arr: number[] = [1, 3, 5, 7, 9];
 //master version2
 function sum(arr: number[]): number {
     let sum:number = 100;
-    arr.forEach(element => {
-        sum-= element;
+    let info:number = 2
+    arr.forEach(e => {
+        sum-= e;
+        info+= e;
     });
-    return sum;
+    return sum+info;
 }
