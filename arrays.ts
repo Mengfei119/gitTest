d702d0d4f651a58972ad8f8269097a1cbc2c8b85
Mengfei Xiong <mx224@cornell.edu>
2020-05-13 23:10:22 -0400

    mxiong version1
    mxiong version2
    
    mxiong version3

--- arrays.ts
+++ arrays.ts
@@ -1,8 +1,14 @@
-const arr: number[] = [1, 2, 3, 4, 5];
+//mxiong version3
+const arr: number[] = [1, 2, 3, 4, 5, 6, 7];
+const num:number = 2;
 function sum(arr: number[]): number {
     let sum:number = 0;
-    arr.forEach(element => {
-        sum+= element;
+    arr.forEach((element, index) => {
+        if(index%2===0){
+            sum+=element;
+        }else{
+            sum-= element;
+        }
     });
-    return sum;
+    return sum + num;
 }
--- test.ts
+++ test.ts
@@ -1,6 +1,11 @@
 const a:number = 1;
 const b:number = 2;
 
-function add1(a:number, b:number): number {
-    return a+b;
+function add(a:number, b:number, c:number): number {
+    //mxiong version 3
+    return a+b-c;
 };
+
+function multi(a:number, b:number): number {
+    return a* b;
+}
